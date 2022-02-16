4089bf91f90515e8e5a3a8ec10d2011b0ae43d1a
yhunlu <yhunlu87@gmail.com>
2022-02-16 22:51:31 +0300

    added postcard and fixed nextjs config for image

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,11 +1,22 @@
+import moment from 'moment';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const PostCard = ({ post }) => {
+  console.log(post);
+
   return (
-    <>
-      <div>{post.title}</div>
-      <div>{post.excerpt}</div>
-    </>
+    <div className="bg-black shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        <Image
+          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+          src={post.featuredImage.url}
+          alt={post.title}
+          layout="fill"
+        />
+      </div>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Categories, PostCard, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-];
-
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +13,7 @@ const Home = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -32,3 +28,11 @@ const Home = () => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
