383017ef1a9ba4aec0d866561b3dac76e45ca790
yhunlu <yhunlu87@gmail.com>
2022-02-23 00:50:01 +0300

    fixed bug for exporting loader

--- components/index.js
+++ components/index.js
@@ -9,3 +9,4 @@ export { default as PostDetail } from './PostDetail';
 export { default as Author } from './Author';
 export { default as AdjacentPostCard } from './AdjacentPostCard';
 export { default as FeaturedPostCard } from './FeaturedPostCard';
+export { default as Loader } from './Loader';
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+  Loader,
+} from '../../components';
 import { getPosts, getPostDetails } from '../../services';
 import AdjacentPosts from './../../sections/AdjacentPosts';
 
@@ -25,7 +33,10 @@ const PostDetails = ({ post }) => {
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">
-              <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+              <PostWidget
+                slug={post.slug}
+                categories={post.categories.map((category) => category.slug)}
+              />
               <Categories />
             </div>
           </div>
